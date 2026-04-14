1edc6016bcd52969c6c8bb7f36b1f1ac4484a4cb
Richard Kwang <43795498+kwangric@users.noreply.github.com>
2026-04-14 15:19:16 +0000

    Initial setup with 11ty

---
+++ eleventy.config.js
@@ -0,0 +1,15 @@
+module.exports = async function (eleventyConfig) {
+	eleventyConfig.addPassthroughCopy({ 'src/assets': 'assets' })
+	eleventyConfig.addPassthroughCopy({ 'src/images': 'images' })
+
+	return {
+		dir: {
+			input: 'src',
+			includes: '_includes',
+			data: '_data',
+			output: '_site',
+		},
+		htmlTemplateEngine: 'liquid',
+		markdownTemplateEngine: 'liquid',
+	}
+}
